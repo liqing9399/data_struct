173e370e3548de58027a65cce046148e02591d30
liqing <362085095@qq.com>
2018-11-17 01:54:20 +0000

    feat(): the second CMakeLists.txt for project
    add .hpp and .cpp for main.cpp

---
+++ mathFunctions.hpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef TEST_MATHFUNCTIONS_HPP
+#define TEST_MATHFUNCTIONS_HPP
+
+
+/**
+ * power - Calculate the power of number.
+ * @param base: Base value.
+ * @param exponent: Exponent value.
+ *
+ * @return base raised to the power exponent.
+ */
+double power(double base, int exponent);
+
+
+#endif
